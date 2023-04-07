63ee1c2fbe822989eec5fce1bc8de8826a4c9de3
Christopher Neugebauer <chrisjrn@toolchain.com>
2023-04-07 12:02:32 -0700

    Make rusty-pants work a bit better in the context of the rest of the Pants codebase

--- build-support/bin/rusty_pants.sh
+++ build-support/bin/rusty_pants.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
+# shellcheck disable=SC1091
+source "${HERE}/../common.sh"
+
+PY="$(determine_python)"
+
 _RUSTY_PANTS_PANTS_SHA=7c3ce8bea5595306582cb95173114d0f24dfd3ae
-_RUSTY_PANTS_CARGO_DIR=`dirname \`which cargo\`` 
-_RUSTY_PANTS_RUSTUP_DIR=`dirname \`which rustup\``
-_RUSTY_PANTS_PYTHON_DIR=`dirname \`which $PY\``
-_RUSTY_PANTS_PYTHON_BINARY=`basename \`which $PY\``
+_RUSTY_PANTS_CARGO_DIR="$(dirname "$(which cargo)")"
+_RUSTY_PANTS_RUSTUP_DIR="$(dirname "$(which rustup)")"
+_RUSTY_PANTS_PYTHON_DIR="$(dirname "$(which "$PY")")"
+_RUSTY_PANTS_PYTHON_BINARY="$(basename "$(which "$PY")")"
 
 export _RUSTY_PANTS_CARGO_DIR
 export _RUSTY_PANTS_RUSTUP_DIR
 export _RUSTY_PANTS_PYTHON_DIR
 export _RUSTY_PANTS_PYTHON_BINARY
+
+if ! command -v pants &> /dev/null; then
+  die "Please install the Pants Native Binary (\"scie-pants\") and ensure \`pants\` is on your PATH."
+fi
 
 PANTS_SHA=${_RUSTY_PANTS_PANTS_SHA} pants export-codegen src/rust/engine:compile_rust
