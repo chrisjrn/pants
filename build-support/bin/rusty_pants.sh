29e475e4557b726e6a57600baaf2aa143c0aebcd
Christopher Neugebauer <chrisjrn@toolchain.com>
2023-04-10 10:30:50 -0700

    floop

--- build-support/bin/rusty_pants.sh
+++ build-support/bin/rusty_pants.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# Copyright 2023 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
 
 HERE=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
 # shellcheck disable=SC1091
@@ -6,7 +8,7 @@ source "${HERE}/../common.sh"
 
 PY="$(determine_python)"
 
-_RUSTY_PANTS_PANTS_SHA=7c3ce8bea5595306582cb95173114d0f24dfd3ae
+_RUSTY_PANTS_PANTS_VERSION=2.17.0.dev1
 _RUSTY_PANTS_CARGO_DIR="$(dirname "$(which cargo)")"
 _RUSTY_PANTS_RUSTUP_DIR="$(dirname "$(which rustup)")"
 _RUSTY_PANTS_PYTHON_DIR="$(dirname "$(which "$PY")")"
@@ -21,4 +23,11 @@ if ! command -v pants &> /dev/null; then
   die "Please install the Pants Native Binary (\"scie-pants\") and ensure \`pants\` is on your PATH."
 fi
 
-PANTS_SHA=${_RUSTY_PANTS_PANTS_SHA} pants export-codegen src/rust/engine:compile_rust
+PANTS_VERSION=${_RUSTY_PANTS_PANTS_VERSION} \
+    PANTS_BACKEND_PACKAGES="[
+        'pants.backend.docker',        
+        'pants.backend.experimental.adhoc',
+        'pants.backend.python',
+        'pants.backend.shell',
+    ]" \
+    pants --no-pantsd --no-verify-config --no-delegate-bootstrap export-codegen src/rust/engine:compile_rust
